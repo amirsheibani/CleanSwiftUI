a1ba209dad9653320b9828eec4edb52c1c858001
amirsheibani <a.sheibani@citydi.com>
2024-10-19 18:17:29 +0330

    add entity

---
+++ TestOne/Features/user_ip/domain/entities/IPEntity.swift
@@ -0,0 +1,10 @@
+struct IPModel: Codable{
+    var ip:String?
+    var country:String?
+    
+    mutating func getObject(value: [String:Any]) -> IPModel{
+        self.ip = value["ip"] as? String
+        self.country = value["country"] as? String
+        return self
+    }
+}
